618d1111e21514b160d043b03f23e138ae73b3ff
alejandro.caceres.dominguez <alejandro.caceres.dominguez@auleseebe.upc.edu>
2025-02-13 13:00:17 +0100

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,32 @@
 #Codigo para problema 1
 
-plot(1:5)
+dim(mtcars)
+
+mis_dades <- mtcars  
+
+mis_dades
+
+mean(mis_dades$qsec)
+
+x <- mis_dades$qsec
+sum(x)/length(x)
+
+
+x <- mis_dades$drat
+
+sort(x)
+median(x)
+quantile(x, 0.25)
+quantile(x, 0.75)
+
+quantile(mis_dades$mpg, 0.18)
+
+quantile(mis_dades$mpg, 0.75)-quantile(mis_dades$mpg, 0.25)
+IQR(mis_dades$mpg)
+
+boxplot(mis_dades$mpg)
+
+IQR(mis_dades$cyl)
+
+sd(mis_dades$cyl)
+var(mis_dades$qsec)
