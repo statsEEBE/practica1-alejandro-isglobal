ee680fbd23a2725f10d3157974bbe822034a10a6
alejandro.caceres.dominguez <alejandro.caceres.dominguez@auleseebe.upc.edu>
2025-02-13 13:42:53 +0100

    ...

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,38 @@
 #Codigo para problema 2
+
+#tabla de frecuencias
+
+data <- mtcars
+data
+
+#discretas
+x <- data$cyl
+ni <- table(x)
+fi <- ni/length(x)
+Ni <- cumsum(ni)
+Fi <- cumsum(fi)
+cbind(ni, fi, Ni, Fi)
+
+
+#continuos
+x <- data$mpg
+bins <- cut(x, 10)
+
+ni <- table(bins)
+fi <- ni/length(bins)
+Ni <- cumsum(ni)
+Fi <- cumsum(fi)
+cbind(ni, fi, Ni, Fi)
+
+#histogram: continuas
+hist(data$mpg)
+hist(data$mpg, breaks = 10)
+
+#barplot : discretas
+ni <- table(data$cyl)
+barplot(ni)
+
+#pie : discretas
+fi <- ni/length(data$cyl)
+pie(fi)
+
--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,10 @@
-#Codigo para problema 3
+#Codigo para problema 
+x <- c(8, 6, 0, 1, 7, 5, 8, 1, 3, 4)
+sum(x)
+
+y <- x[-c(3,7,8)]
+
+sum(exp(x))-sum(exp(y))
+
+y[5]
+y[2]
