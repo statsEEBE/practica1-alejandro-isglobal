c10b61f90cb2c7b86ab361a03e3c7bbd3ab5ace7
alejandro.caceres.dominguez <alejandro.caceres.dominguez@auleseebe.upc.edu>
2025-02-18 09:21:18 +0100

    ...

--- Problema1.R
+++ Problema1.R
@@ -1,33 +1,38 @@
 #Codigo para problema 1
-
-dim(mtcars)
-
-mis_dades <- mtcars  
-
-mis_dades
-
-mean(mis_dades$qsec)
+mis_dades <- mtcars
+mis_dades 
+dim(mis_dades)
+names(mis_dades)
 
 x <- mis_dades$qsec
 sum(x)/length(x)
+mean(x)
 
-
-x <- mis_dades$drat
-
-sort(x)
-median(x)
-quantile(x, 0.25)
-quantile(x, 0.75)
+mis_dades$drat
+sort(mis_dades$drat)
+median(mis_dades$drat)
+quantile(mis_dades$drat, 0.25)
+quantile(mis_dades$drat, 0.75)
 
 quantile(mis_dades$mpg, 0.18)
 
+quantile(mis_dades$mpg, 0.25)
+quantile(mis_dades$mpg, 0.75)
+sort(mis_dades$mpg)
+median(mis_dades$mpg)
 quantile(mis_dades$mpg, 0.75)-quantile(mis_dades$mpg, 0.25)
 IQR(mis_dades$mpg)
 
 boxplot(mis_dades$mpg)
 
+median(mis_dades$cyl)
 IQR(mis_dades$cyl)
+mean(mis_dades$cyl)
+
 
 sd(mis_dades$cyl)
 var(mis_dades$qsec)
-<asfzasdfed
+x <- mis_dades$qsec
+
+sum((x-mean(x))^2)/(length(x)-1)
+var(x)
