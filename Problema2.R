9dc9b10b23908f576df4993a1d175e391acf0416
Alejandro Caceres <alejandro.caceres@isglobal.org>
2025-02-18 09:50:47 +0100

    Update Problema2.R
    ....

--- Problema2.R
+++ Problema2.R
@@ -1,38 +1,35 @@
 #Codigo para problema 2
 
-#tabla de frecuencias
+mis_dades <- mtcars
+x <- mis_dades$cyl
 
-data <- mtcars
-data
+#frecuencia absoluta
+ni <- table(x)
+barplot(ni)
+#frecuencia relativa
+fi <- ni/length(x)
+pie(fi)
+#frecuencia acumulada
+Ni <- cumsum(ni)
+#frecuencia relaiva acumulada
+Fi <- cumsum(fi)
+fi
+Fi
 
-#discretas
-x <- data$cyl
 ni <- table(x)
 fi <- ni/length(x)
 Ni <- cumsum(ni)
 Fi <- cumsum(fi)
 cbind(ni, fi, Ni, Fi)
 
-
-#continuos
-x <- data$mpg
-bins <- cut(x, 10)
-
-ni <- table(bins)
-fi <- ni/length(bins)
+mis_dades$mpg
+x <- cut(mis_dades$mpg, 10)
+ni <- table(x)
+fi <- ni/length(x)
 Ni <- cumsum(ni)
 Fi <- cumsum(fi)
 cbind(ni, fi, Ni, Fi)
 
-#histogram: continuas
+data <- mis_dades
 hist(data$mpg)
-hist(data$mpg, breaks = 10)
-
-#barplot : discretas
-ni <- table(data$cyl)
-barplot(ni)
-
-#pie : discretas
-fi <- ni/length(data$cyl)
-pie(fi)
 
